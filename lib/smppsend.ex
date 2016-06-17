f41f0b87e922b17671c81285c1ed74204092f73b
av <av@fun-box.ru>
2016-06-18 01:08:32 +0300

    Refactored TLV converting functions into seperate module

--- lib/smppsend.ex
+++ lib/smppsend.ex
@@ -189,45 +189,12 @@ Example:
     System.halt(code)
   end
 
-  defp convert_tlvs(_options, _res \\ [], _tlvs \\ [])
-
-  defp convert_tlvs([], res, tlvs), do: [{:tlvs, tlvs} | res]
-  defp convert_tlvs([{:tlvs, _} | rest], res, tlvs), do: convert_tlvs(rest, res, tlvs)
-  defp convert_tlvs([{k, v} | rest], res, tlvs) do
-    re = ~r/^tlv_(?:(?<hex_id>x[\da-fA-F]{4})|(?<name>[a-z\_]+))_(?<value_type>s|i(?<int_value_size>1|2|4|8)|h)$/
-    matches = Regex.named_captures(re, to_string(k))
-    case matches do
-      nil -> convert_tlvs(rest, [{k, v} | res], tlvs)
-      %{"hex_id" => hex_id, "name" => name, "value_type" => value_type, "int_value_size" => int_value_size} ->
-        convert_tlvs(rest, res, [{tlv_id(k, hex_id, name), tlv_value(k, value_type, int_value_size, v)} | tlvs])
+  defp convert_tlvs(opts) do
+    case SMPPSend.TlvParser.convert_tlvs(opts) do
+      {:ok, opts} -> opts
+      {:error, message, key} ->
+        error!(1, "Error parsing tlv option #{key |> original_key}: #{message}")
     end
-  end
-
-  defp tlv_value(key, <<"i", _>>, int_value_size_s, value) do
-    {int_value_size, ""} = Integer.parse(int_value_size_s)
-    bit_value_size = 8 * int_value_size
-    case Integer.parse(value) do
-      {int, ""} -> <<int :: big-unsigned-integer-size(bit_value_size)>>
-      _ -> error!(1, "Bad integer tlv value (#{inspect value}) for key #{key |> original_key}")
-    end
-  end
-  defp tlv_value(_key, "s", _, value), do: value
-  defp tlv_value(key, "h", _, value) do
-    case Base.decode16(value, case: :mixed) do
-      {:ok, val} -> val
-      :error -> error!(1, "Bad hex tlv value (#{inspect value}) for key #{key |> original_key}")
-    end
-  end
-
-  defp tlv_id(key, "", name) do
-    case name |> String.to_atom |> SMPPEX.Protocol.TlvFormat.id_by_name do
-      {:ok, id} -> id
-      :unknown -> error!(1, "Unknown tlv name (#{name}) in key #{key |> original_key}")
-    end
-  end
-  defp tlv_id(_key, << "x", hex_id :: binary>>, "") do
-    {:ok, << int :: big-unsigned-integer-size(16) >>} = Base.decode16(hex_id, case: :mixed)
-    int
   end
 
   defp original_key(key) do
---
+++ lib/smppsend/tlv_parser.ex
@@ -0,0 +1,51 @@
+defmodule SMPPSend.TlvParser do
+
+  @tlv_re ~r/^tlv_(?:(?<hex_id>x[\da-fA-F]{4})|(?<name>[a-z\_]+))_(?<value_type>s|i(?<int_value_size>1|2|4|8)|h)$/
+
+  def convert_tlvs(_options, _res \\ [], _tlvs \\ [])
+
+  def convert_tlvs([], res, tlvs), do: {:ok, [{:tlvs, Enum.reverse(tlvs)} | Enum.reverse(res)]}
+  def convert_tlvs([{:tlvs, _} | rest], res, tlvs), do: convert_tlvs(rest, res, tlvs)
+  def convert_tlvs([{key, value} | rest], res, tlvs) do
+    matches = Regex.named_captures(@tlv_re, to_string(key))
+    case matches do
+      nil -> convert_tlvs(rest, [{key, value} | res], tlvs)
+      %{"hex_id" => hex_id, "name" => name, "value_type" => value_type, "int_value_size" => int_value_size} ->
+        case tlv_id(hex_id, name) do
+          {:ok, tlv_id} ->
+            case tlv_value(value_type, int_value_size, value) do
+              {:ok, tlv_value} -> convert_tlvs(rest, res, [{tlv_id, tlv_value} | tlvs])
+              {:error, error} -> {:error, error, key}
+            end
+          {:error, error} -> {:error, error, key}
+        end
+    end
+  end
+
+  defp tlv_value(<<"i", _>>, int_value_size_s, value) do
+    {int_value_size, ""} = Integer.parse(int_value_size_s)
+    bit_value_size = 8 * int_value_size
+    case Integer.parse(value) do
+      {int, ""} -> {:ok, <<int :: big-unsigned-integer-size(bit_value_size)>>}
+      _ -> {:error, "bad integer tlv value (#{inspect value})"}
+    end
+  end
+  defp tlv_value("s", _, value), do: {:ok, value}
+  defp tlv_value("h", _, value) do
+    case Base.decode16(value, case: :mixed) do
+      {:ok, val} -> {:ok, val}
+      :error -> {:error, "bad hex tlv value (#{inspect value})"}
+    end
+  end
+
+  defp tlv_id("", name) do
+    case name |> String.to_atom |> SMPPEX.Protocol.TlvFormat.id_by_name do
+      {:ok, id} -> {:ok, id}
+      :unknown -> {:error, "unknown tlv name (#{name})"}
+    end
+  end
+  defp tlv_id(<< "x", hex_id :: binary>>, "") do
+    {:ok, << int :: big-unsigned-integer-size(16) >>} = Base.decode16(hex_id, case: :mixed)
+    {:ok, int}
+  end
+end
