2cb4794632b414ba9cb47db8bd61cdb7e91d1fb2
av <av@fun-box.ru>
2017-01-23 20:58:11 +0300

    SMPPEX update && version bumpup

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Smppsend.Mixfile do
   def project do
     [
       app: :smppsend,
-      version: "0.1.4",
+      version: "0.1.5",
       elixir: "~> 1.1",
       build_embedded: Mix.env == :prod,
       start_permanent: Mix.env == :prod,
