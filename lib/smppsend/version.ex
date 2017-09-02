53b7c6a84e07099a36fb152387dc41d8795134f1
av <av@fun-box.ru>
2017-09-03 00:43:56 +0300

    OTP 17 compatible --version option

--- lib/smppsend/version.ex
+++ lib/smppsend/version.ex
@@ -1,7 +1,11 @@
 defmodule SMPPSend.Version do
   def version do
-    Application.spec(:smppsend)
-    |> Keyword.get(:vsn)
+    Application.loaded_applications
+    |> List.keyfind(:smppsend, 0)
+    |> case do
+      {_, _, vsn} -> vsn
+      _ -> "unknown"
+    end
     |> to_string
   end
 end
