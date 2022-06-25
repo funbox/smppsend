fc490ec651ab33f29a0d9ecaed219e140eecbfd8
Ilya Averyanov <av@rubybox.ru>
2022-06-25 19:49:36 +0300

    use only Elixir versions that are actual for SMPPEX

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Smppsend.Mixfile do
     [
       app: :smppsend,
       version: "0.1.16",
-      elixir: "~> 1.5",
+      elixir: "~> 1.7",
       build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
       elixirc_paths: elixirc_paths(Mix.env()),
