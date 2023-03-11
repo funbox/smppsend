5d9e5e76b62aab9d8ed98cd379e1fc11a387d82e
Ilya Averyanov <av@rubybox.dev>
2023-03-11 22:38:55 +0200

    add Burrito build targets and scripts

---
+++ burrito/build.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+set -xe
+
+mix deps.get
+mix compile
+
+otp_version=$(mix burrito.otp.version)
+ssl_version=$(mix burrito.ssl.version)
+
+OTP_VERSION="${otp_version}" ARCH=x86_64  OPENSSL_VERSION="${ssl_version}" ./burrito/download/macos.sh
+OTP_VERSION="${otp_version}" ARCH=aarch64 OPENSSL_VERSION="${ssl_version}" ./burrito/download/macos.sh
+
+OTP_VERSION="${otp_version}" ARCH=x86_64  LIBC=gnu OPENSSL_VERSION="${ssl_version}" ./burrito/download/linux.sh
+OTP_VERSION="${otp_version}" ARCH=aarch64 LIBC=gnu OPENSSL_VERSION="${ssl_version}" ./burrito/download/linux.sh
+
+mix release smppsend --overwrite
